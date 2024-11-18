30326cd21c9f54fa358d815dec59af903427f36c
GabrielPHB <gabrielrsmachado50@gmail.com>
2024-11-18 10:57:34 -0300

    First Commit

---
+++ todo_list/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//void main() => runApp(MyApp());
+
+class ToDo_List extends StatelessWidget {
+  const ToDo_List({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ToDo_List();
+  }
+}
